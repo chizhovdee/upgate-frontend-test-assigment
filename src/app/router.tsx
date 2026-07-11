4d1b2a3d72bcfe5cca9b86f02a25522ce31ce4ef
Dmitry Chizhov <chizhov.dee@gmail.com>
2026-07-11 16:49:07 +0300

    feat(checkout): add checkout page stub

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -6,12 +6,17 @@ const CatalogPage = lazy(() =>
   import('features/catalog').then((module) => ({ default: module.CatalogPage })),
 );
 
+const CheckoutPage = lazy(() =>
+  import('features/checkout').then((module) => ({ default: module.CheckoutPage })),
+);
+
 function withSuspense(element: ReactNode) {
   return <Suspense fallback={<div>Loading...</div>}>{element}</Suspense>;
 }
 
 export const routes = [
   { path: '/', element: withSuspense(<CatalogPage />) },
+  { path: '/checkout', element: withSuspense(<CheckoutPage />) },
 ] satisfies RouteObject[];
 
 export const router = createBrowserRouter(routes);
